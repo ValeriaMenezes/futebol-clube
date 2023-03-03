6d05c66c7c28a9859f28c8d09db0acd6ee3d885f
Valéria <valeriamenezes022@gmail.com>
2023-03-03 09:01:57 -0300

    feat: Desenvolvido uma migration e um model para a tabela de partidas

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,54 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './TeamsModel';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: INTEGER,
+    },
+    homeTeamId: {
+      allowNull: false,
+      type: INTEGER,
+    },
+    homeTeamGoals: {
+      allowNull: false,
+      type: INTEGER,
+    },
+    awayTeamId: {
+      allowNull: false,
+      type: INTEGER,
+    },
+    awayTeamGoals: {
+      allowNull: false,
+      type: INTEGER,
+    },
+    inProgress: {
+      allowNull: false,
+      type: BOOLEAN,
+    },
+  },
+  {
+    sequelize: db,
+    underscored: true,
+    modelName: 'matches',
+    timestamps: false,
+  },
+);
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+export default Matches;
